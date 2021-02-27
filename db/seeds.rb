277fcfee5d69eb7ad096143f390adf454d763e25
Alejandro Calzadilla <alejandrocalzadilla@gmail.com>
2021-02-27 12:09:24 -0500

    uncommented stars

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,10 @@ Ingredient.destroy_all
 
 puts "creating database"
 
-
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+puts "creating ingredients"
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients = JSON.parse(open(url).read)
+ingredients["drinks"].each do |ingredient|
+  i = Ingredient.create(name: ingredient["strIngredient1"])
+  puts "create #{i.name}"
+end
